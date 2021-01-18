e42ba68ca554c428818d3266b22f36d7a5ea840e
kreeshn <vikram.dharajiya143@gmail.com>
2021-01-18 07:46:43 +0530

    Add Retrofit Classes in Admin Module

--- freshersliveadmin/src/main/java/com/example/freshersliveadmin/activity/MainActivity.java
+++ freshersliveadmin/src/main/java/com/example/freshersliveadmin/activity/MainActivity.java
@@ -1,8 +1,10 @@
-package com.example.freshersliveadmin;
+package com.example.freshersliveadmin.activity;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+
+import com.example.freshersliveadmin.R;
 
 public class MainActivity extends AppCompatActivity {
 
---
+++ freshersliveadmin/src/main/java/com/example/freshersliveadmin/retrofit/Api.java
@@ -0,0 +1,4 @@
+package com.example.freshersliveadmin.retrofit;
+
+public interface Api {
+}
